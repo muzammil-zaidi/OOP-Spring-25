58be9c5c676016d5483dabcad3f2f0f6a9e77463
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-07 22:21:29 +0500

    Update Task 4.cpp

--- LAB 2/Task 4.cpp
+++ LAB 2/Task 4.cpp
@@ -1,27 +1,41 @@
 #include<iostream>
-#include<string>
-#include<algorithm>
+#include<cctype>
 using namespace std;
 
+string lower(string s){
+	string str=s;
+		for(int i=0;i<str.length();i++){ 
+			str[i]=tolower(str[i]);
+		}
+		return str;
+}
+
 int main(){
-    int n;
-    cout<<"Enter the number of strings: ";
-    cin>>n;
-    cin.ignore();
-    string* strArray=new string[n];
+	int n;
+	string a;
+	cout<<"Enter number of strings: ";
+	cin>>n;
+	cout<<endl;
+	string *s=new string[n];
+	for(int i=0;i<n;i++){
+		cout<<"String "<<i+1<<" : ";
+		cin>>s[i];
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(lower(s[i]).compare(lower(s[j]))<0){
+			a=s[i];
+			s[i]=s[j];
+			s[j]=a;
+			}
+		}
 
-    for(int i=0;i<n;i++){
-        cout<<"Enter string "<<i+1<<": ";
-        getline(cin,strArray[i]);
-    }
-
-    sort(strArray,strArray+n);
-
-    cout<<"\nSorted Strings:\n";
-    for(int i=0;i<n;i++){
-        cout<<strArray[i]<<endl;
-    }
-
-    delete[] strArray;
-    return 0;
+	}
+	cout<<"\n--------------------------------\n";
+	cout<<"Sorted String: "<<endl;
+	for(int i=0;i<n;i++) {
+		cout<<s[i]<<"\n";
+	}
+	delete[] s;
+	return 0;
 }
