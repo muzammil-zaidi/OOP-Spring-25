1dd70d95869904a71d8641e92831f3ff90f6391e
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-16 03:28:40 +0500

    Update Task 1.cpp

--- LAB 3/Task 1.cpp
+++ LAB 3/Task 1.cpp
@@ -2,50 +2,72 @@
 #include<string>
 using namespace std;
 
-class Car{
-	string brand;
-	string model;
-	float price;
-	bool availability;
-	
-	public:
-		Car(){
-			brand="Unknown";
-			model="Generic";
-			price=0.0;
-			availability=true;
-		}
-		
-		Car(string a,string b,float c,bool d){
-			brand=a;
-			model=b;
-			price=c;
-			availability=d;
-		}
-		
-		void availability(string a,string b){
-			if(availability==true){
-				cout<<"Available!";
-			}
-		}
-		
-		void request(int request){
-				if(availability==false){
-				cout<<"Unavailable!";
-			}
-		}
-		
-		void display(){
-			cout<<"Brand: "<<brand<<endl;
-			cout<<"Model: "<<model<<endl;
-			cout<<"Price: "<<price<<endl;
-			cout<<"Availibilty: "<<availibilty<<endl;
-		}
+class Wallet{
+    private:
+        string ownerName;
+        float totalBalance;
+        string transactionHistory[50];
+        int count;
+
+    public:
+        Wallet(){
+            ownerName="";
+            totalBalance=0;
+            count=0;
+        }
+
+        Wallet(string name){
+            ownerName=name;
+            totalBalance=0;
+            count=0;
+        }
+        
+        void add(float amount){
+            totalBalance+=amount;
+            if(count<50){
+                transactionHistory[count]="Deposited: "+to_string(amount);
+                count++;
+            }
+        }
+        
+        void spend(float amount){
+            if(amount>0 && amount<=totalBalance){
+                totalBalance-=amount;
+                if(count<50){
+                    transactionHistory[count]="Spent: "+ to_string(amount);
+                    count++;
+                }
+                cout<<"Transaction successful!\n";
+                if(totalBalance<500){
+                    cout<<"Warning! low balance.\n";
+                }
+            }
+            else{
+                cout<<"Invalid input!\n";
+            }
+        }
+        void display(){
+            cout << "\n--- Transactions for "<<ownerName<<" ---\n";
+            cout<<"---------------------------------\n";
+            for(int i=0;i<count;i++){
+                cout<<transactionHistory[i]<<"\n";
+            }
+            cout<<"Current Balance: "<<totalBalance<<"\n";
+        }   
 };
 
 int main(){
-	Car c;
-	c.Car("Corrolla","BA-23",232455.00,true);
-	c.display();
-	return 0;
+    string name;
+    cout<<"Enter your name: ";
+    getline(cin,name);
+    
+    Wallet w(name);
+    w.add(25000);
+    w.spend(8000);
+    w.display();
+    w.add(5000);
+    w.spend(15000);
+    w.display();
+    
+    return 0;
 }
