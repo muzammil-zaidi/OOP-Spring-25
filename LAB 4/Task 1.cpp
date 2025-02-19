59913202c10968bf5bfac03bb3e0afa0ff71dff4
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-02-20 00:18:47 +0500

    Update Task 1.cpp

--- LAB 4/Task 1.cpp
+++ LAB 4/Task 1.cpp
@@ -3,49 +3,83 @@
 using namespace std;
 
 class Car{
-	string brand;
-	string model;
-	float price;
-	bool availability;
-	
-	public:
-		Car(){
-			brand="Unknown";
-			model="Generic";
-			price=0.0;
-			availability=true;
-		}
-		
-		Car(string a,string b,float c,bool d){
-			brand=a;
-			model=b;
-			price=c;
-			availability=d;
-		}
-		
-		void availability(string a,string b){
-			if(availability==true){
-				cout<<"Available!";
-			}
-		}
-		
-		void request(int request){
-				if(availability==false){
-				cout<<"Unavailable!";
-			}
-		}
-		
-		void display(){
-			cout<<"Brand: "<<brand<<endl;
-			cout<<"Model: "<<model<<endl;
-			cout<<"Price: "<<price<<endl;
-			cout<<"Availibilty: "<<availibilty<<endl;
-		}
+    string brand;
+    string model;
+    float price;
+    bool availability;
+
+public:
+    Car(){
+        brand="Unknown";
+        model="Generic";
+        price=0.0;
+        availability=true;
+    }
+
+    Car(string b,string m,float p,bool a){
+        cout<<"\nUpdating car details...\n";
+        cout<<"------------------------"<<endl;
+        brand=b;
+        model=m;
+        price=p;
+        availability=a;
+    }
+
+    void checkAvailability(){
+        cout<< "\nChecking availability...\n";
+        cout<<"------------------------"<<endl;
+        if(availability){
+            cout<<"Car is Available!"<<endl;
+        }
+        else{
+            cout<<"Car is Unavailable!"<<endl;
+        }
+    }
+
+    void rentCar(){
+        cout<<"\nProcessing rental request...\n";
+        cout<<"------------------------"<<endl;
+        if(availability){
+            availability=false;
+            cout<<"Car has been rented successfully!"<<endl;
+        }
+        else{
+            cout<<"Car is already rented!"<<endl;
+        }
+    }
+
+    void setDetails(string b,string m,float p,bool a){
+        brand=b;
+        model=m;
+        price=p;
+        availability=a;
+    }
+
+    void display(){
+        cout<< "\nDisplaying car details...\n";
+        cout<<"------------------------"<<endl;
+        cout<<"Brand: "<<brand<<endl;
+        cout<<"Model: "<< model<<endl;
+        cout<<"Price: $"<<price<<endl;
+        cout<<"Availability: ";
+        if(availability){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
 };
 
 int main(){
-	Car c;
-	c.Car("Corrolla","BA-23",232455.00,true);
-	c.display();
-	return 0;
+    Car c;
+    
+    c.display();
+    c.setDetails("Corolla","BA-23",232455.00,true);
+    c.display();
+    c.checkAvailability();
+    c.rentCar();
+    c.checkAvailability();
+    
+    return 0;
 }
