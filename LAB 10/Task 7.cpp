fe4425bde1f405c493cc5e84e39b820ef39f5b23
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-04 01:04:44 +0500

    Update Task 7.cpp

--- LAB 10/Task 7.cpp
+++ LAB 10/Task 7.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<fstream>
+#include<string>
 
+int main(){
+    std::ofstream outFile("sensor_log.txt");
+
+    if(!outFile){
+        std::cerr<<"Error: Could not open file for writing.\n";
+        return 1;
+    }
+
+    outFile<<"Sensor 1: 23.5 C\n";
+    std::streampos p1=outFile.tellp();
+    std::cout<<"Position after first write: "<<p1<<std::endl;
+
+    outFile<<"Sensor 2: 25.1 C\n";
+    std::streampos p2=outFile.tellp();
+    std::cout<<"Position after second write: "<<p2<<std::endl;
+
+    outFile.close();
+    return 0;
+}
