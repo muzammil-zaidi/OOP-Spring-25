c51de529fde701ec391d9641848411a4378f27bc
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-04 00:28:32 +0500

    Update Task 1.cpp

--- LAB 10/Task 1.cpp
+++ LAB 10/Task 1.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<fstream>
+#include<string>
 
+int main(){
+    std::ifstream vehicleFile;
+    
+    vehicleFile.open("vehicles.txt");
+    
+    if(!vehicleFile.is_open()){
+        std::cerr<<"Error: Unable to open the file 'vehicles.txt'"<<std::endl;
+        return 1;
+    }
+    
+    std::cout<<"File opened successfully!"<<std::endl;
+    
+    std::string line;
+    while(std::getline(vehicleFile, line)){
+        if (line.empty() || line[0]=='#'){
+            continue;
+        }
+        
+        std::cout<<"Read line: "<<line<<std::endl;
+    }
+    
+    vehicleFile.close();
+    
+    return 0;
+}
