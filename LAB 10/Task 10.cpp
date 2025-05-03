c28ed6b305f406b8d9e892a0bd614dcf4b4564e7
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-04 01:49:22 +0500

    Update Task 10.cpp

--- LAB 10/Task 10.cpp
+++ LAB 10/Task 10.cpp
@@ -5,7 +5,7 @@
 int main(){
     std::ofstream outFile("data_records.txt");
     if(!outFile){
-        std::cerr<<"Error: Could not create the file."<<std::endl;
+        std::cerr<<"Error: Could not create the file.\n";
         return 1;
     }
 
@@ -17,18 +17,19 @@ int main(){
 
     std::ifstream inFile("data_records.txt");
     if(!inFile){
-        std::cerr<<"Error: Could not open the file for reading."<<std::endl;
+        std::cerr<<"Error: Could not open file for reading.\n";
         return 1;
     }
+
+
+    inFile.seekg(0);
 
     std::string line;
     std::getline(inFile,line);
     std::getline(inFile,line);
-
     std::getline(inFile,line);
     std::cout<<"Third Record: "<<line<<std::endl;
 
     inFile.close();
-
     return 0;
 }
