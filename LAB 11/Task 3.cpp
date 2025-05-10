d93b1fef8cc49e576477e63eb381bd361d676fb2
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-05-10 17:57:15 +0500

    Update Task 3.cpp

--- LAB 11/Task 3.cpp
+++ LAB 11/Task 3.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<exception>
+using namespace std;
 
+class InvalidTemperatureException : public exception{};
+
+template<typename T>
+T convertToFahrenheit(T celsius){
+    if(celsius<-273.15){
+        throw InvalidTemperatureException();
+    }
+    return (celsius*9/5)+32;
+}
+
+int main(){
+    try{
+        cout<<"Attempting to convert -300C ..."<<endl;
+        double result=convertToFahrenheit(-300);
+        cout<<"Fahrenheit: "<<result<<endl;
+    }
+    catch(const InvalidTemperatureException& e){
+        cerr<<"Caught InvalidTemperatureException - what(): "<<e.what()<<endl;
+    }
+
+    return 0;
+}
